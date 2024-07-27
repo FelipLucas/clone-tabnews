ef984a6694cb0f2977d440753ed1c3c8b6f82af8
Felipe <fefelbf@gmail.com>
2024-07-27 00:04:19 +0000

    adiciona o arquivos js.config.json

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils/index.js";
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
  
 async function status(request, response){
-  const result = await database.query("SELECT 1 + 1");
-  console.log(result.rows)
+  const result = await database.query("SELECT 1 + 1 as sum");
+  console.log(result.rows); 
   response.status(200).json({chave: "são acima da media"})
 }
 export default status;
