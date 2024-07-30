8a33cb51941099e4f8d6571b31fe8dd22d722946
Felipe <fefelbf@gmail.com>
2024-07-30 13:43:13 +0000

    adiciona arquvis de teste

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils/index.js";
+//import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils/index.js";
 import database from "infra/database.js";
  
 async function status(request, response){
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows); 
-  response.status(200).json({chave: "são acima da media"})
+  const uptadedAt = new Date().toISOString();
+
+  response.status(200).json({
+  });
 }
+
 export default status;
