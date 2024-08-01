99e62ea39e927272f6a4c2c976fbbec7411e74e3
Felipe <fefelbf@gmail.com>
2024-08-01 12:36:48 +0000

    adicona arquivos ao indewx.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 //import { RESPONSE_LIMIT_DEFAULT } from "next/dist/server/api-utils/index.js";
-import database from "infra/database.js";
+import database from "infra/database";
  
 async function status(request, response){
   const uptadedAt = new Date().toISOString();
 
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const  dataBaseVersionValue = databaseVersionResult.rows[0].server_version;
   response.status(200).json({
+    uptaded_at: uptadedAt,
+    dependencies: {
+      database: {
+        version: dataBaseVersionValue,
+      }
+    }
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,5 +3,10 @@ test("GET to /api/v1/status should return 200", async () =>{
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.uptaded_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.uptaded_at).toBeDefined();
+
+  const parsedUptadedAt = new Date(responseBody.uptaded_at).toISOString();
+  expect(responseBody.uptaded_at).toEqual(parsedUptadedAt);
+
+  expect(responseBody.dependencies.version).toEqual("16.0");
 });
