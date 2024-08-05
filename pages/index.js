f2f17406abf083344e7e22016e9ed9fe927e1cb0
Felipe <fefelbf@gmail.com>
2024-08-05 13:18:11 +0000

    adiciona arquivos ao banco de dados

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,15 @@ async function status(request, response){
 
   const databaseVersionResult = await database.query("SHOW server_version;");
   const  dataBaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query("SHOW max_connections");
+  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
   response.status(200).json({
     uptaded_at: uptadedAt,
     dependencies: {
       database: {
         version: dataBaseVersionValue,
+        max_connections: databaseMaxConnectionsValue,
       }
     }
   });
