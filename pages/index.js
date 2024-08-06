87575bb9fe43756378256c23a361e67955814287
Felipe <fefelbf@gmail.com>
2024-08-06 12:34:52 +0000

    modifica o page index.js

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,24 @@ async function status(request, response){
   const uptadedAt = new Date().toISOString();
 
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const  dataBaseVersionValue = databaseVersionResult.rows[0].server_version;
+  const  databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections");
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+  const databaseMaxConnectionsValue = databaseOpenedConnectionsResult.rows[0].max_connections;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';"
+  );
+
+  const databaseOpenedConnectionsValue = 
+  databaseOpenedConnectionsResult.rows[0].count;
+
   response.status(200).json({
     uptaded_at: uptadedAt,
     dependencies: {
       database: {
-        version: dataBaseVersionValue,
-        max_connections: databaseMaxConnectionsValue,
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       }
     }
   });
